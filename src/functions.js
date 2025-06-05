ff394d2ac3e6cf581a26ac4dd0b04c08ba0cb350
Antonin Simek <antonin.simek@etnetera.cz>
2025-06-05 12:26:42 +0200

    Using FontFamily from swiftgen

--- src/functions.js
+++ src/functions.js
@@ -5,9 +5,49 @@
  * 
  * @returns {string}
  */
- function createDocumentationComment(text, indentationString) {
-    // Add the [indentationString] to all but the first line
-    return text.trim().split("\n").map((line, index) => ((index > 0) ? `${indentationString}` : ``) + `/// ${line}`).join("\n")
+function createDocumentationComment(text, indentationString) {
+    return text
+        .trim()
+        .split("\n")
+        .map((line, index) =>
+            (index > 0 ? `${indentationString}` : ``) + `/// ${line}`
+        )
+        .join("\n");
 }
 
-Pulsar.registerFunction("createDocumentationComment", createDocumentationComment)
+Pulsar.registerFunction("createDocumentationComment", createDocumentationComment);
+
+/**
+ * Sanitizes a font subfamily (e.g. "Semi Bold" -> "semiBold")
+ * 
+ * @param {string} name 
+ * @returns {string}
+ */
+function sanitizeSubfamily(name) {
+    return name
+        .split(/\s+/)
+        .map((part, index) =>
+            index === 0
+                ? part.charAt(0).toLowerCase() + part.slice(1)
+                : part.charAt(0).toUpperCase() + part.slice(1)
+        )
+        .join('');
+}
+
+Pulsar.registerFunction("sanitizeSubfamily", sanitizeSubfamily);
+
+/**
+ * Sanitizes a font family (e.g. "Chakra Petch" -> "ChakraPetch")
+ * @param {string} name 
+ * @returns {string}
+ */
+function sanitizeFamily(name) {
+    return name
+        .split(/\s+/)
+        .map(part =>
+            part.charAt(0).toUpperCase() + part.slice(1)
+        )
+        .join('');
+}
+
+Pulsar.registerFunction("sanitizeFamily", sanitizeFamily);
